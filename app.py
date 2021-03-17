a1e2ac1db052035430bbb5d0ff77242bd03b2624
JosephMugo <joemu18@gmail.com>
2021-03-17 03:18:04 -0500

    feat - add movie and actor models
    movie and actor models added for database

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from models import setup_db
 
 def create_app(test_config=None):
   # create and configure the app
@@ -11,6 +12,7 @@ def create_app(test_config=None):
   return app
 
 APP = create_app()
+setup_db(APP)
 
 if __name__ == '__main__':
     APP.run(host='0.0.0.0', port=8080, debug=True)
--- models.py
+++ models.py
@@ -1,1 +1,25 @@
+from flask_sqlalchemy import SQLAlchemy
+import json
+import os
 
+db = SQLAlchemy()
+
+def setup_db(app):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    release_date = db.Column(db.Date, nullable=False)
+    actors = db.relationship('Actor', backref='movie', lazy=True)
+
+class Actor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.String, nullable=False)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'), nullable=False)
