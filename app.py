0a4a7f831e85bfe9884ebfdd2f5e9281f6cb24c0
JosephMugo <joemu18@gmail.com>
2021-03-18 01:57:38 -0500

    feat: error handler
    added error handlers

--- app.py
+++ app.py
@@ -14,5 +14,25 @@ def create_app(test_config=None):
 
 APP = create_app()
 
+'''
+Error Handler 
+'''
+
+@APP.errorhandler(404)
+def page_not_found(e):
+  return jsonify(status_code=400, error="Not Found"), 400
+
+@APP.errorhandler(403)
+def page_not_found(e):
+  return jsonify(status_code=403, error="Forbidden"), 403
+
+@APP.errorhandler(410)
+def page_not_found(e):
+  return jsonify(status_code=410, error="Gone"), 410
+
+@APP.errorhandler(500)
+def page_not_found(e):
+  return jsonify(status_code=500, error="Internal Server Error"), 500
+
 if __name__ == '__main__':
     APP.run(host='0.0.0.0', port=8080, debug=True)
