c5215c23a3992d4438390b6b66dbedef2fa2f27f
JosephMugo <joemu18@gmail.com>
2021-03-18 00:57:17 -0500

    chore - setup render template

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, abort, jsonify
+from flask import Flask, request, abort, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from models import setup_db
