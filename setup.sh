42baf83644869b983fa73027e1ff7574b70c88fd
JosephMugo <joemu18@gmail.com>
2021-05-06 08:57:53 -0500

    removed setup.sh from gitignore

---
+++ setup.sh
@@ -0,0 +1,5 @@
+DATABASEURL=postgresql://ziktogyafulmyh:5a9fc2d7b207ae473a18a0f4f9ad61cf81f360473d4af8daf02a16c95fbbbb27@ec2-54-161-239-198.compute-1.amazonaws.com:5432/d1ae8k9rsrgtca
+AUTH0_DOMAIN=dev-8bzdf01x.us.auth0.com
+AUTH0_CLIENT_ID=kcqDRJaQO1VsrHAR9zzWiYb4upMADUdI
+ALGORITHMS=RS256
+API_AUDIENCE=casting
